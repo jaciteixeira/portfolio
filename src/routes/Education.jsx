54056f64d99afab9b002a58e60cf908d6aba6d2e
jaciteixeira <rm99627@fiap.com.br>
2024-02-06 12:52:48 -0300

    atualizado certificados

--- src/routes/Education.jsx
+++ src/routes/Education.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import Certificado from '../components/Certificado';
-import certificadoFiap from '../assets/certificado/certificado-fiap.svg';
-import certificadoFiapWeb from '../assets/certificado/certificado-fiap-web.svg';
-import certificadoLibras from '../assets/certificado/certificado-libras.png';
-import certificadoSustentabilidade from '../assets/certificado/certificado-sustentabilidade.png';
-import certificadoSwagger from '../assets/certificado/certificado-swagger.png';
-import certificadoReact from '../assets/certificado/certificado-react.png';
-import certificadoReactRouter from '../assets/certificado/certificado-react-router-dom.png';
-import certificadoReactComponents from '../assets/certificado/certificado-react-components.png';
-import certificadoImpactaAplicacao from '../assets/certificado/certificado-impacta-aplicacao-java.svg';
-import certificadoImpactaDesenvolvimento from '../assets/certificado/certificado-impacta-desenvolvimento-web-service.svg';
+// certificados fiap
+import certificadoFiap from '../assets/certificado/certificado-fiap.png';
+import certificadoFiapWeb from '../assets/certificado/certificado-fiap-web.png';
+import certificadoLibras from '../assets/certificado/certificado-fiap-libras.png';
+import certificadoSustentabilidade from '../assets/certificado/certificado-fiap-sustentabilidade.png';
+//certificados alura
+import certificadoSwagger from '../assets/certificado/certificado-alura-swagger.png';
+import certificadoReact from '../assets/certificado/certificado-alura-react.png';
+import certificadoReactRouter from '../assets/certificado/certificado-alura-react-router-dom.png';
+import certificadoReactComponents from '../assets/certificado/certificado-alura-react-components.png';
+import certificadoJavaServlet from '../assets/certificado/certificado-alura-java-servlet.png';
+//certificado impacta
+import certificadoFormacaoBackend from '../assets/certificado/certificado-impacta-formacao-backend-developer.png';
+import certificadoAplicacao from '../assets/certificado/certificado-impacta-aplicacao-java.png';
+import certificadoDesenvolvimento from '../assets/certificado/certificado-impacta-desenvolvimento-web-service.png';
+import certificadoJira from '../assets/certificado/certificado-impacta-jira-software.png';
+import certificadoLiderança from '../assets/certificado/certificado-impacta-lideranca-essencial-para-lider.png';
+import certificadoScrum from '../assets/certificado/certificado-impacta-scrum-gestao-desenvolvimentoa-gil.png';
+import certificadoProdutividade from '../assets/certificado/certificado-impacta-produtividade-gestao-tempo.png';
+import certificadoPLSQL from '../assets/certificado/certificado-impacta-programando-com-mySQL.png';
+import certificadoMySql from '../assets/certificado/certificado-impacta-mySQL-criando-manipulando.png';
+import certificadoJavaWeb from '../assets/certificado/certificado-impacta-java-web.png';
+import certificadoPOO from '../assets/certificado/certificado-impacta-introducao-POO.png';
+import certificadoLogica from '../assets/certificado/certificado-impacta-logica-programacao-aplicada.png';
 
 export default function Education() {
   const certificadosFiap = [
@@ -61,8 +75,14 @@ export default function Education() {
     {
       url: 'https://cursos.alura.com.br/user/jaci9306/course/React-desenvolvendo-react-router-javaScript/certificate',
       alt: 'Certificado Curso REACT: DESENVOLVENDO EM REACT ROUTER COM JAVASCRIPT',
-      src: certificadoReactComponents,
+      src: certificadoReactRouter,
       descricao: 'React: Como os componentes funcionam',
+    },
+    {
+      url: 'https://cursos.alura.com.br/user/jaci9306/course/servlets-fundamentos-programacao-web-java/certificate',
+      alt: 'Certificado Curso Java Servlet: Programação WEB JAVA',
+      src: certificadoJavaServlet,
+      descricao: 'Curso Java Servlet: Programação WEB JAVA',
     },
 
   ]
@@ -70,15 +90,75 @@ export default function Education() {
   const certificadosImpacta = [
     {
       url: 'https://edools-3-production.s3.amazonaws.com/org-6352/school-9201/certificates/enrollment-8873698/course-14854-0862e031-5724-41a2-aa21-263f573bc6bc.pdf',
-      alt: 'Certificado Java - Desenvolvendo Web Services',
-      src: certificadoImpactaDesenvolvimento,
+      alt: 'Certificado Certificado Java - Desenvolvendo Web Services',
+      src: certificadoDesenvolvimento,
       descricao: 'Java - Desenvolvendo Web Services',
     },
     {
       url: 'https://edools-3-production.s3.amazonaws.com/org-6352/school-9201/certificates/enrollment-8873698/course-14852-30d51589-8726-4cd3-9b00-1aca3307ecd3.pdf',
-      alt: 'Java - Criando uma Aplicação Completa ',
-      src: certificadoImpactaAplicacao,
+      alt: 'Certificado Java - Criando uma Aplicação Completa ',
+      src: certificadoAplicacao,
       descricao: 'Java - Criando uma Aplicação Completa',
+    },
+    {
+      url: 'https://edools-3-production.s3.amazonaws.com/org-6352/school-9201/certificates/enrollment-8873698-abb3c652-d9d9-4570-9b75-42d1652813ff.pdf',
+      alt: 'Certificado Formação Back-End Developer',
+      src: certificadoFormacaoBackend,
+      descricao: 'Formação Back-End Developer',
+    },
+    {
+      url: 'https://edools-3-production.s3.amazonaws.com/org-6352/school-9201/certificates/enrollment-8873698/course-14840-7704b436-f98e-4147-a4c9-dce831e5c720.pdf',
+      alt: 'Certificado Java Web',
+      src: certificadoJavaWeb,
+      descricao: 'Java Web',
+    },
+    {
+      url: 'https://edools-3-production.s3.amazonaws.com/org-6352/school-9201/certificates/enrollment-8873698/course-14819-2938b6da-8640-49b5-bdcc-364d67637575.pdf',
+      alt: 'Certificado Introdução à Programação Orientada a Objeto',
+      src: certificadoPOO,
+      descricao: 'Introdução à Programação Orientada a Objeto',
+    },
+    {
+      url: 'https://edools-3-production.s3.amazonaws.com/org-6352/school-9201/certificates/enrollment-8873698/course-25121-86891e1b-3a01-4252-9578-ce5e068d1ef6.pdf',
+      alt: 'Certificado Lógica de Programação Aplicada à Linguagem ',
+      src: certificadoLogica,
+      descricao: 'Lógica de Programação Aplicada à Linguagem ',
+    },
+    {
+      url: 'https://edools-3-production.s3.amazonaws.com/org-6352/school-9201/certificates/enrollment-8873698/course-44575-272b69cb-01be-455b-93e8-3f007004dae1.pdf',
+      alt: 'Certificado Jira Software - Gestão Ágil de Projetos e Operações',
+      src: certificadoJira,
+      descricao: 'Jira Software - Gestão Ágil de Projetos e Operações',
+    },
+    {
+      url: 'https://edools-3-production.s3.amazonaws.com/org-6352/school-9201/certificates/enrollment-8873698/course-14863-f34d1282-3d14-44cf-a1e9-e9a54be27708.pdf',
+      alt: 'Certificado Scrum - Gestão e Desenvolvimento Ágil de Projetos',
+      src: certificadoScrum,
+      descricao: 'Scrum - Gestão e Desenvolvimento Ágil de Projetos',
+    },
+    {
+      url: 'https://edools-3-production.s3.amazonaws.com/org-6352/school-9201/certificates/enrollment-8873698/course-19497-9fb9862d-18e4-45bd-b5ee-98051731f33a.pdf',
+      alt: 'Certificado MySQL - Criando e Manipulando Banco de Dados',
+      src: certificadoMySql,
+      descricao: 'MySQL - Criando e Manipulando Banco de Dados',
+    },
+    {
+      url: 'https://edools-3-production.s3.amazonaws.com/org-6352/school-9201/certificates/enrollment-8873698/course-32260-3936dc63-7e6c-4982-b67d-760890dcc5f8.pdf',
+      alt: 'Certificado MySQL PL SQL - Programando com MySQL ',
+      src: certificadoPLSQL,
+      descricao: 'MySQL PL SQL - Programando com MySQL',
+    },
+    {
+      url: 'https://edools-3-production.s3.amazonaws.com/org-6352/school-9201/certificates/enrollment-8873698/course-141858-a51fda7e-279c-4cf9-9c4e-7d9f93473fa2.pdf',
+      alt: 'Certificado Liderança - O Essencial para o Líder',
+      src: certificadoLiderança,
+      descricao: 'Liderança - O Essencial para o Líder',
+    },
+    {
+      url: 'https://edools-3-production.s3.amazonaws.com/org-6352/school-9201/certificates/enrollment-8873698/course-156551-2f4d28da-517f-423d-b7a3-c58c5071ce8c.pdf',
+      alt: 'Certificado Produtividade e Gestão do Tempo ',
+      src: certificadoProdutividade,
+      descricao: 'Produtividade e Gestão do Tempo ',
     },
   ]
 
