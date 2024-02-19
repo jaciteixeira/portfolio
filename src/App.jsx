8179c9761652c2e3562cdac247d1df611f17b994
jaciteixeira <rm99627@fiap.com.br>
2024-02-19 14:39:24 -0300

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,13 @@
 import "./styles/App.scss";
 
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { Outlet, } from "react-router-dom";
 
 import BackToTopButton from "./components/BackToTopButton";
 import Footer from "./components/Footer.jsx";
 import Header from "./components/Header.jsx";
 import WhatsappIcon from "./components/WhatsappIcon";
-import About from './routes/About.jsx';
-import Education from "./routes/Education.jsx";
-import Home from './routes/Home.jsx';
-import NotFound from './routes/NotFound.jsx';
 
-import {
-  Route,
-  BrowserRouter as Router,
-  Routes,
-} from "react-router-dom";
 
 export default function App() {
   
@@ -33,27 +24,13 @@ export default function App() {
   return (
 
   <div className="container">
-    <Router>
       <Header/>
-      <Routes>
-        <Route path= "/" element= {<Home/>}/>
-        <Route path= "/home" element= {<Home/>}/>
-        <Route path= "/portfolio" element= {<Home/>}/>
-        <Route path= "/about" element= {<About/>}/>
-        <Route path= "/education" element= {<Education/>}/>
-        <Route path= "*" element= { <NotFound/> }/>
-      </Routes>
+    <Outlet/>
       <section id="icons-rodape">
         <WhatsappIcon />
         <BackToTopButton />
       <Footer />
       </section>
-    </Router>
   </div>
   );
 }
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>
-)
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css';
 import AppRoutes from './App.jsx';
+import Home from './routes/Home.jsx'
+import About from './routes/About.jsx'
+import Education from './routes/Education.jsx'
+import Error from './routes//NotFound.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <AppRoutes />
-  </React.StrictMode>
-);
+const router = createBrowserRouter(
+  [
+    {path: '/', element: <AppRoutes/>,
+    errorElement: <Error/>,
+    children:[
+      { path: '/', element: <Home/> },
+      { path: '/about', element: <About/> },
+      { path: '/education', element: <Education/> },
+      ]
+    }
+
+  ]
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  
+  <RouterProvider router={router} />
+  
+)
